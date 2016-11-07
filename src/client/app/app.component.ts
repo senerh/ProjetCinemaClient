fca4a7f1444a3952916322384aff87b51744faf1
nathanFrancois <nat36200@gmail.com>
2016-11-07 09:01:06 +0100

    salut

--- src/client/app/app.component.ts
+++ src/client/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { Config } from './shared/index';
-import './operators';
 
 /**
  * This class represents the main application component. Within the @Routes annotation is the configuration of the
